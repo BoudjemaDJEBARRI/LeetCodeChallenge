61e72607def0cd54c7a1614de62af87378e4e274
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-02-15 17:14:35 +0100

    Leet Code Challenges

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -6,14 +6,14 @@ public class TwoSum {
 
         int[] nums = {2, 4, 5, 8, 3};
         int target = 7;
+        // 2+5=7
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    System.out.println(nums[i] + " & " + nums[j]);
+                    System.out.println(nums[i] + " index: " + i + " & " + nums[j]+ " index: " + j);
                 }
             }
         }
-
     }
 }
--- src/TwoSumMeth2.java
+++ src/TwoSumMeth2.java
@@ -5,20 +5,44 @@ public class TwoSumMeth2 {
     public static void main(String[] args) {
         int[] arrNum = {2, 4, 5, 8, 3};
         int target = 7;
-        //  System.out.println(arrNum[i]);
+
+        System.out.println(twoSum1(arrNum, 7));
+        System.out.println(twoSum2(arrNum, 7));
     }
-    public int[] twoSum1(int[] nums, int target) {
-        Map<Integer, Integer> complements = new HashMap<>();
+
+    public static int[] twoSum1(int[] nums, int target) {
+
+        Map<Integer, Integer> map1 = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            Integer complementIndex = complements.get(nums[i]);
-            if (complementIndex != null) {
-                return new int[]{i, complementIndex};
+            Integer index1 = map1.get(nums[i]);
+            if (index1 != null) {
+                return new int[]{i, index1};
             }
-            complements.put(target-nums[i], i);
+            map1.put(target - nums[i], i);
         }
-        throw new IllegalArgumentException("NoNum");
+        throw new IllegalArgumentException("No Num");
     }
-        /*
+      public static int[] twoSum2(int[] nums, int target) {
+
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map2.put(nums[i], i);
+        }
+          for (int i = 0; i < nums.length; i++) {
+              int diff2 = target - nums[i];
+              if (map2.containsKey(diff2) && map2.get(diff2) != i);
+              return new int[] {map2.get(diff2)};
+          }
+
+          return nums;
+    }
+
+
+}
+
+/*
     public int[] twoSum(int[] arrNum, int target) {
         for (int i = 0; i < arrNum.length; i++) {
             for (int j = i + 1; j < arrNum.length; j++) {
@@ -29,6 +53,4 @@ public class TwoSumMeth2 {
         }
         throw new IllegalArgumentException("NoNum");
     }
-
-         */
-}
+*/
